4af07630d1cb6b311a668eea2bb3627d4409dc86
YonatanFinerman <julianmode808@gmail.com>
2023-03-14 18:53:43 +0200

    a

--- src/cmps/guide.jsx
+++ src/cmps/guide.jsx
@@ -6,17 +6,17 @@ import { Fade } from "react-reveal";
 export function Guide() {
 
     const [currStepIdx, setCurrStepIdx] = useState(0)
-    const steps = [1, 2, 1, 2, 1]
+    const steps = [0, 1, 2, 3, 4]
 
 
     return <section className="guide flex column align-center text-center" id="guide">
 
         <div className="step-carousel flex" >
 
-            {steps.map(imgUrl => {
+            {steps.map((imgUrl,idx) => {
 
                 return <div  className='guide-step flex align-center justify-center'
-                    style={{ transform: `translateX(-${currStepIdx * 100}%)`, backgroundImage: `url(${require(`../assets/img/hero${imgUrl}.jpg`)})` }} >
+                    style={{ transform: `translateX(-${currStepIdx * 100}vw)`, backgroundImage: `url(${require(`../assets/img/step${idx}.jpg`)})` }} >
                     <Fade bottom> <h2>hi djkASKADjaskDJASkjk</h2> </Fade>
                 </div>
 
